b436e0042ccee3db02e953db0f62b4dbbe2ed234
Заян Дандляев <zayandandlyaev@MacBook-Air-Zaan.local>
2022-03-20 13:14:54 +0300

    lasttime

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -108,7 +108,6 @@ WSGI_APPLICATION = 'api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -117,14 +116,6 @@ DATABASES = {
         'PASSWORD': 'postgres',
         'HOST': 'db',
         'PORT': 5432
-    }
-}
-"""
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
