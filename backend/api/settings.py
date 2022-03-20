697355b4f00da60ed3ce12d83c3d44868c581a75
Заян Дандляев <zayandandlyaev@MacBook-Air-Zaan.local>
2022-03-20 14:11:25 +0300

    wegddd

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -108,6 +108,7 @@ WSGI_APPLICATION = 'api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -118,6 +119,17 @@ DATABASES = {
         'PORT': 5432
     }
 }
+
+"""
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+"""
 
 
 # Password validation
