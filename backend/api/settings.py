5d73310101be94e614344e2e1699c8744f27a128
Zayan <zayandandlyaev@yandex.ru>
2022-03-23 16:52:52 +0300

    gogo

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -126,13 +126,7 @@ DATABASES = {
     }
 }
 '''
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': os.environ.get(
@@ -145,7 +139,7 @@ DATABASES = {
         'PORT': os.environ.get('DB_PORT'),
     }
 }
-'''
+
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
