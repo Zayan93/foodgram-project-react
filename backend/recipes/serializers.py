2fe35a65728840999be9eeebc72ff310902d2db5
Заян Дандляев <zayandandlyaev@MacBook-Air-Zaan.local>
2022-03-25 00:43:31 +0300

    create

--- backend/recipes/serializers.py
+++ backend/recipes/serializers.py
@@ -125,6 +125,21 @@ class RecipeFullSerializer(serializers.ModelSerializer):
         self.create_bulk(recipe, ingredients_data)
         return recipe
 
+    @transaction.atomic
+    def update(self, instance, validated_data):
+        ingredients_data = validated_data.pop('ingredients')
+        tags_data = validated_data.pop('tags')
+        IngredientAmount.objects.filter(recipe=instance).delete()
+        self.create_bulk(instance, ingredients_data)
+        instance.name = validated_data.pop('name')
+        instance.text = validated_data.pop('text')
+        instance.cooking_time = validated_data.pop('cooking_time')
+        if validated_data.get('image') is not None:
+            instance.image = validated_data.pop('image')
+        instance.save()
+        instance.tags.set(tags_data)
+        return instance
+
     def validate(self, data):
         ingredients = self.initial_data.get('ingredients')
         ingredient_list = []
@@ -146,21 +161,6 @@ class RecipeFullSerializer(serializers.ModelSerializer):
                 'Время приготовления должно быть больше 0'
             )
         return data
-
-    @transaction.atomic
-    def update(self, instance, validated_data):
-        ingredients_data = validated_data.pop('ingredients')
-        tags_data = validated_data.pop('tags')
-        IngredientAmount.objects.filter(recipe=instance).delete()
-        self.create_bulk(instance, ingredients_data)
-        instance.name = validated_data.pop('name')
-        instance.text = validated_data.pop('text')
-        instance.cooking_time = validated_data.pop('cooking_time')
-        if validated_data.get('image') is not None:
-            instance.image = validated_data.pop('image')
-        instance.save()
-        instance.tags.set(tags_data)
-        return instance
 
 
 class FavoriteSerializer(serializers.ModelSerializer):
